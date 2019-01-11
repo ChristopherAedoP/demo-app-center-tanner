92970b5a02e3a7badf28c3262a1f31431b15538f
Christopher Aedo <christopher.aedo.p@gmail.com>
2019-01-11 13:11:39 -0300

    version 1,0,3

--- App.js
+++ App.js
@@ -24,7 +24,7 @@ export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text style={styles.welcome}>Welcome to React Native! appcenter ok 1.0.2 (3)</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
       </View>
